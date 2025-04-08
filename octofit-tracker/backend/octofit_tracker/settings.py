d9764e5bbd4b51ec76709754c0b83a8d1f43fdad
Graham <grahamrgriffiths@gmail.com>
2025-04-08 10:24:48 +0000

    Fix assertion

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
-# Djongo setup
+# djongo setup
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
